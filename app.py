5176591e654297bb06caecf798a575e2bee44d1d
hamdanhh07 <99419105+hamdanhh07@users.noreply.github.com>
2023-02-07 02:05:12 +0300

    Update app.py

--- app.py
+++ app.py
@@ -6,10 +6,10 @@ example_list =  [['examples/cov1.png'],
                  ['examples/penu1.jpg'],
                  ['examples/penu2.jpg']]
 from huggingface_hub.inference_api import InferenceApi
-inference = InferenceApi(repo_id="hamdan07/UltraSound-Lung", token="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")
+
 
 
 title = "COVID-19 Detection in Ultrasound Imagery Using Artificial intelligent Methods"
 description = "[Trained on european car plates] Identifies the license plate, cuts and displays it, and converts it into text. An image with higher resolution and clearer license plate will have a better accuracy."
 
-gr.Interface.load("models/hamdan07/UltraSound-Lung",examples=example_list,title=title,description=description).launch(debug=False,share=False)
+gr.Interface.load(inference = InferenceApi(repo_id="hamdan07/UltraSound-Lung", token="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"),examples=example_list,title=title,description=description).launch(debug=False,share=False)
