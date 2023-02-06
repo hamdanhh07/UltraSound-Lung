cd3704550b0d0836e338615ff75464e8cd51b10e
hamdanhh07 <99419105+hamdanhh07@users.noreply.github.com>
2023-02-07 01:43:00 +0300

    Update app.py

--- app.py
+++ app.py
@@ -1,13 +1,13 @@
 import gradio as gr
-example_list =  [['examples/1.png'],
-                 ['examples/2.png'],
-                 ['examples/3.png'],
-                 ['examples/4.png'],
-                 ['examples/5.png'],
-                 ['examples/6.png']]
+example_list =  [['examples/cov1.png'],
+                 ['examples/cov2.jpg'],
+                 ['examples/nor1.png'],
+                 ['examples/nor2.jpg'],
+                 ['examples/penu1.jpg'],
+                 ['examples/penu2.jpg']]
 
 title = "License plate reader 🚌"
 description = "[Trained on european car plates] Identifies the license plate, cuts and displays it, and converts it into text. An image with higher resolution and clearer license plate will have a better accuracy."
 
-gr.Interface.load("models/swww/test",examples=example_list,title=title,description=description).launch(debug=False,share=False)
+gr.Interface.load("models/hamdan07/UltraSound-Lung",examples=example_list,title=title,description=description).launch(debug=False,share=False)
 
