3500fe876a013cb5d8015f6fb7af4966b79f70be
hamdanhh07 <99419105+hamdanhh07@users.noreply.github.com>
2023-02-07 02:53:25 +0300

    Update app.py

--- app.py
+++ app.py
@@ -15,33 +15,23 @@ example_list =  [['examples/cov1.png'],
                  ['examples/nor2.jpg'],
                  ['examples/penu1.jpg'],
                  ['examples/penu2.jpg']]
-title = "COVID-19 Detection in Ultrasound Imagery Using Artificial intelligent Methods"
-description = "[Trained on 500 data using Hugging Face dataset."
-app = gr.Blocks()
-with app:
-  
-    gr.Markdown("# **<p align='center'>Video Classification with Timesformer</p>**")
-    gr.Markdown(
-        """
+title ="# **<p align='center'>COVID-19 Detection in Ultrasound with Timesformer</p>**"
+description ="""
         <p style='text-align: center'>
-        Timesformer is a video model that uses a Transformer architecture to process video frames.
+        Trained on 500 data using Hugging Face dataset..
         <br>It is released by Facebook AI Research in ICML 2021.
-        <br>This version is trained on Kinetics-400 dataset and can classify videos into 400 classes.
-        </p>
-        """
-    )
-    gr.Markdown(
-        """
+        <br>      
         <p style='text-align: center'>
         Follow me for more! 
         <br> <a href='https://twitter.com/fcakyon' target='_blank'>twitter</a> | <a href='https://github.com/fcakyon' target='_blank'>github</a> | <a href='https://www.linkedin.com/in/fcakyon/' target='_blank'>linkedin</a> | <a href='https://fcakyon.medium.com/' target='_blank'>medium</a>
         </p>
+        .
+        </p>
         """
-    )
+
+
+   
     
    
-   
-    
-   
-gr.Interface.load("models/hamdan07/UltraSound-Lung",examples=example_list,title=app).launch(debug=False,share=False)
+gr.Interface.load("models/hamdan07/UltraSound-Lung",examples=example_list,title=title,description=description).launch(debug=False,share=False)
 
