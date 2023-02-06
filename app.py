ae988c4c105c09cb47dc93cd9558ec7407f359aa
hamdanhh07 <99419105+hamdanhh07@users.noreply.github.com>
2023-02-07 02:14:01 +0300

    Update app.py

--- app.py
+++ app.py
@@ -19,4 +19,4 @@ def query(filename):
 title = "COVID-19 Detection in Ultrasound Imagery Using Artificial intelligent Methods"
 description = "[Trained on european car plates] Identifies the license plate, cuts and displays it, and converts it into text. An image with higher resolution and clearer license plate will have a better accuracy."
 
-gr.Interface.load("models/hamdan07/UltraSound-Lung"),examples=example_list,title=title,description=description).launch(debug=False,share=False)
+gr.Interface.load("models/hamdan07/UltraSound-Lung",examples=example_list,title=title,description=description).launch(debug=False,share=False)
