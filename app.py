a07e1f22b7d1e9acbf3dab0ae20f904d260e8e3e
hamdanhh07 <99419105+hamdanhh07@users.noreply.github.com>
2023-02-08 00:29:16 +0300

    Update app.py

--- app.py
+++ app.py
@@ -1,9 +1,22 @@
 import gradio as gr
 import requests
+from datasets import load_dataset
 from transformers import AutoFeatureExtractor, AutoModelForImageClassification
+from transformers import ViTFeatureExtractor, ViTForImageClassification
+from PIL import Image
+import requests
 extractor = AutoFeatureExtractor.from_pretrained("google/vit-base-patch16-224")
 model = AutoModelForImageClassification.from_pretrained("google/vit-base-patch16-224")
 
+dataset = load_dataset("hamdan07/UltraSound-lung")
+image = Image.open(requests.get(dataset, stream=True).raw)
+feature_extractor = ViTFeatureExtractor.from_pretrained('google/vit-base-patch16-224')
+model = ViTForImageClassification.from_pretrained('google/vit-base-patch16-224')
+inputs = feature_extractor(images=image, return_tensors="pt")
+outputs = model(**inputs)
+logits = outputs.logits
+predicted_class_idx = logits.argmax(-1).item()
+print("Predicted class:", model.config.id2label[predicted_class_idx])
 API_URL = "https://api-inference.huggingface.co/models/hamdan07/UltraSound-Lung"
 headers = {"Authorization": "Bearer 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"}
 
